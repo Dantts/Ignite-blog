453d468209b2c598113363df286627baba24cc4c
Gabriel Duarte <gabriel.dantas@parnamirim.rn.gov.br>
2022-06-13 10:41:06 -0300

    feat: Iniciando layout e listagem estatica dos posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/Link';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.headerContainer}>
+      <h1>
+        <Link href="/">
+          <img src="/images/Logo.svg" alt="Logo" />
+        </Link>
+      </h1>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +25,54 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | SpaceTravaling</title>
+      </Head>
+      <main>
+        {postsPagination.results.map(post => (
+          <section key={post.uid}>
+            <h1>{post.data.title}</h1>
+            <p>{post.data.subtitle}</p>
+            <div>
+              <time>{post.first_publication_date}</time>
+              <span>{post.data.author}</span>
+            </div>
+          </section>
+        ))}
+      </main>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('ignews-publication');
 
-//   // TODO
-// };
+  const postsResults = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title[0].text,
+        subtitle: 'Adicionar depois',
+        author: 'Dantts',
+      },
+    };
+  });
+
+  const postsPagination: PostPagination = {
+    next_page: postsResponse.next_page,
+    results: postsResults,
+  };
+
+  console.log(JSON.stringify(postsResponse, null, 2));
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
